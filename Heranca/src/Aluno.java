0670d3ab9f1c6fb59cf02c2025f84c5ec0821d17
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2020-04-26 20:47:23 -0300

    add herança

---
+++ Heranca/src/Aluno.java
@@ -0,0 +1,48 @@
+/*
+    a classe Aluno herda a classe Pessoa, pois tem em comum 
+    os atributos
+
+*/
+public class Aluno extends Pessoa{
+    private String curso;
+    private double[] notas;
+    
+    // this  --> faz referencia a propria classe
+    // super --> faz referencia a super classe, nesse caso Pessoa
+    public Aluno (String nome, String endereco, String telefone, String curso){
+        super (nome, endereco, telefone);
+        this.curso = curso;
+    }
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+    
+    public double calcularMedia (){
+        return 0;
+    }
+    
+    public boolean verificarAprovado (){
+        return true;
+    }
+    
+    public void metodoQualquer (){
+        // this tem acesso a todos elementos tanto da sua classe
+        // quanto da super classe
+        this.getCpf();
+        // ja super tem acesso apenas aos elementos da super classe
+        super.getCpf();
+    }
+    
+}
---
+++ Heranca/src/heranca.java
@@ -0,0 +1,38 @@
+/*
+    * modificadores de acesso:
+        modificador -> acesso
+
+        + public      -> classe, subclasse, pacote, todos (pacotes, classes)
+        - protected   -> classe, subclasse, pacote
+        ! default     -> classe, pacote
+        # private     -> classe
+        
+    * abstract
+        uma classe abstrata nao pode ser instanciada diretamente:
+            NomeClasse objeto = new NomeClasse();                      [ x ]
+        ela só pode ser extendida por outras classes:
+            public class NomeClasse extends NomeSuperClasseAbstrata {} [ v ]
+
+   * exemplo
+    Animal <- [ Ave <- (Papagaio), Mamifero <- (Cachorro, Gato) ]
+        superClasse mae    public abstract class Animal {}
+        superClasse filha  public abstract class Ave extends Animal {}
+        superClasse filha  public abstract class Mamifero extends Animal {}
+        classe filha       public class Papagaio extends Ave {}
+        classe filha       public class Cachorro extends Mamifero {}
+        classe filha       public class Gato extends Mamifero {}
+
+*/
+public class heranca {
+    public static void main (String[] args){
+        
+        Aluno aluno = new Aluno("rodrigo", "rua tal", "0000", "curso");
+        aluno.getNome();
+        // uma super classe, já que Pessoa é a classe mãe, eu posso fazer:
+        
+        Pessoa aluno2 = new Aluno("rodrigo", "rua tal", "0000", "curso");
+        // mas a super classe só da acesso ao que é comum entre as classes
+    
+        
+    }
+}
